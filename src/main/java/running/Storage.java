3a424f29eeb104f3db7185d4b1b43a7fcfe127c1
Matthan Foo <matthanfoo@gmail.com>
2025-02-16 20:00:27 +0800

    Rewrite files in Storage.java slightly to match coding standard.

--- src/main/java/running/Storage.java
+++ src/main/java/running/Storage.java
@@ -15,33 +15,44 @@ public class Storage {
         this.filename = filename;
     }
 
+    /**
+     * this function takes in a string array of items in a row downloaded from a csv, and creates a task with the relevant information
+     * @param   row     a string array containing items corresponding to the columns in the csv for a row
+     * @return          a task created with the relevant information
+     * @throws Exception if the task is not either a todo, deadline, or an event
+     */
+    public Task readRowToTask(String[] row) throws Exception {
+        Task t;
+        if (row[0].equals("T")) {
+            t = new Todo(row[1]);
+            if (row[2].equals("X")) { t.mark(); }
+        } else if (row[0].equals("E")) {
+            t = new Event(row[1], LocalDateTime.parse(row[3], dateTimeFormatter), LocalDateTime.parse(row[3], dateTimeFormatter));
+            if (row[2].equals("X")) { t.mark(); }
+        } else if (row[0].equals("D")) {
+            t = new Deadline(row[1], LocalDateTime.parse(row[3], dateTimeFormatter));
+            if (row[2].equals("X")) { t.mark(); }
+        } else {
+            throw new Exception("Invalid task");
+        }
+
+        return t;
+    }
+
     public ArrayList<Task> load() {
         File file = new File(filename);
         ArrayList<Task> userInputs = new ArrayList<Task>();
 
         if (file.exists()) {
             System.out.println("File exists, reading contents...");
-            // read CSV into Task ArrayList
 
             try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
                 String line;
                 while ((line = br.readLine()) != null) {
                     String[] row = line.split(","); // Splitting by comma
-                    if (row[0].equals("T")) {
-                        Todo t = new Todo(row[1]);
-                        if (row[2].equals("X")) { t.mark(); }
-                        userInputs.add(t);
-                    } else if (row[0].equals("E")) {
-                        Event e = new Event(row[1], LocalDateTime.parse(row[3], dateTimeFormatter), LocalDateTime.parse(row[3], dateTimeFormatter));
-                        if (row[2].equals("X")) { e.mark(); }
-                        userInputs.add(e);
-                    } else if (row[0].equals("D")) {
-                        Deadline d = new Deadline(row[1], LocalDateTime.parse(row[3], dateTimeFormatter));
-                        if (row[2].equals("X")) { d.mark(); }
-                        userInputs.add(d);
-                    }
+                    userInputs.add(readRowToTask(row));
                 }
-            } catch (IOException e) {
+            } catch (Exception e) {
                 System.out.println("Error reading the file.");
                 e.printStackTrace();
             }
